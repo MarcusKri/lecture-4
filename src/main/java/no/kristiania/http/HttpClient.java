8ec865110bd34f9f36fb7fcc5160ea542750a768
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-05 21:32:44 +0200

    Test shouldReturnStatusCode - green

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,10 +1,35 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class HttpClient {
-    public HttpClient(String host, int port, String requestTarget) {
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port); //Socket = connecter til en server (host og porten som er angitt)
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Connection: close\r\n" +
+                "Host: " + host + "\r\n" +
+                "\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        String[] statusLine = readLine(socket).split(" ");
+        this.statusCode = Integer.parseInt(statusLine[1]);
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder result = new StringBuilder();
+        int c;
+        while((c = socket.getInputStream().read()) != '\r') {
+            result.append((char)c);
+        }
+        return result.toString();
     }
 
     public int getStatusCode() {
-        return 200;
+        return statusCode;
     }
 }
