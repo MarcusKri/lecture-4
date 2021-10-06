1a60afeecba51bd83bc25ad093ecdf952c070151
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-06 11:30:46 +0200

    Server should return content type

--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
@@ -26,7 +27,10 @@ public class HttpServerTest {
     void shouldRespondWith200ForKnownRequestTarget() throws IOException {
         HttpServer server = new HttpServer(10004);
         HttpClient client = new HttpClient("localhost", server.getPort(), "/hello");
-        assertEquals(200, client.getStatusCode());
-        assertEquals("Hello world", client.getMessageBody());
+        assertAll(
+                () -> assertEquals(200, client.getStatusCode()),
+                () -> assertEquals("text/html", client.getHeader("Content-Type")),
+                () -> assertEquals("Hello world", client.getMessageBody())
+        );
     }
 }
